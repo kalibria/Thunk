71a7f40e8fb4eb6aa69c4a9cb0edce25be2a4e7a
Maria Kazakova <mariakazakova92@gmail.com>
2024-08-19 17:12:34 +0300

    added custom hook useAppDispatch

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,6 +2,7 @@ import { tasksReducer } from './tasks-reducer';
 import { todolistsReducer } from './todolists-reducer';
 import {AnyAction, combineReducers, legacy_createStore} from 'redux';
 import {ThunkDispatch} from 'redux-thunk'
+import {useDispatch} from "react-redux";
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -15,6 +16,8 @@ export const store = legacy_createStore(rootReducer);
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
 export type AppDispatchType =ThunkDispatch<AppRootStateType, unknown, AnyAction>
+
+export const  useAppDispatch = useDispatch<AppDispatchType>;
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
