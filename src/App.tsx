9dd895de6d88366df8dc330e30b7eb34789ad56e
Maria Kazakova <mariakazakova92@gmail.com>
2024-08-19 17:08:48 +0300

    added type of Dispatch

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ import {
 } from './state/todolists-reducer'
 import { addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC } from './state/tasks-reducer';
 import { useDispatch, useSelector } from 'react-redux';
-import { AppRootStateType } from './state/store';
+import {AppDispatchType, AppRootStateType} from './state/store';
 import {TaskStatuses, TaskType, todolistsAPI} from './api/todolists-api'
 
 
@@ -34,7 +34,7 @@ function App() {
 
     const todolists = useSelector<AppRootStateType, Array<TodolistDomainType>>(state => state.todolists)
     const tasks = useSelector<AppRootStateType, TasksStateType>(state => state.tasks)
-    const dispatch = useDispatch();
+    const dispatch = useDispatch<AppDispatchType>();
 
     const removeTask = useCallback(function (id: string, todolistId: string) {
         const action = removeTaskAC(id, todolistId);
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,6 +1,7 @@
 import { tasksReducer } from './tasks-reducer';
 import { todolistsReducer } from './todolists-reducer';
-import {combineReducers, createStore, legacy_createStore} from 'redux';
+import {AnyAction, combineReducers, legacy_createStore} from 'redux';
+import {ThunkDispatch} from 'redux-thunk'
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -12,6 +13,8 @@ const rootReducer = combineReducers({
 export const store = legacy_createStore(rootReducer);
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
+
+export type AppDispatchType =ThunkDispatch<AppRootStateType, unknown, AnyAction>
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
